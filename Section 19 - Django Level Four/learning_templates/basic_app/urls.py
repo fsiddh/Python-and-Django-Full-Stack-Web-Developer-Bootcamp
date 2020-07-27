8a8009f99aa95567e159c86337d197cf5c70aa63
Falansh Siddh <jasonshaw546@gmail.com>
2020-07-27 17:02:03 +0530

    Updates

--- Section 19 - Django Level Four/learning_templates/basic_app/urls.py
+++ Section 19 - Django Level Four/learning_templates/basic_app/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from basic_app import views
+from . import views
 
+# SET THE NAMESPACE!
 app_name = 'basic_app'
 
-urlpatterns = [
-    path('relative/', views.relative, name='relative'),
-    path('other/', views.other, name='other'),
-    path('index/', views.index, name='index')
+urlpatterns=[
+    path('relative/',views.relative,name='relative'),
+    path('other/',views.other,name='other'),
 ]
