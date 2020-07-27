3b2043345d977bfb2846de1f6a665b3a6f53bd8b
Falansh Siddh <jasonshaw546@gmail.com>
2020-07-27 11:46:32 +0530

    Updates

--- Section 18 - Django Level Three/ProTwo/appTwo/views.py
+++ Section 18 - Django Level Three/ProTwo/appTwo/views.py
@@ -1,22 +1,19 @@
 from django.shortcuts import render
-from .forms import NewUserForm
+from django.http import HttpResponse
+from .models import User
 
-def index(request):
-    return render(request,'appTwo/index.html')
+# Create your views here.
+# def index(request):
+#     return HttpResponse("<em>My Second Project</em>")
 
 def help(request):
     helpdict = {'help_insert':'HELP PAGE'}
     return render(request,'appTwo/help.html',context=helpdict)
 
+def index(request):
+    return render(request,'appTwo/index.html')
+
 def users(request):
-    form = NewUserForm()
-
-    if request.method == "POST":
-        form = NewUserForm(request.POST)
-        if form.is_valid():
-            form.save(commit=True)
-            return index(request)
-        else:
-            print('ERROR FORM INVALID')
-
-    return render(request,'appTwo/users.html',{'form':form})
+    user_list = User.objects.order_by('first_name')
+    user_dict = {"users":user_list}
+    return render(request,'appTwo/users.html',context=user_dict)
