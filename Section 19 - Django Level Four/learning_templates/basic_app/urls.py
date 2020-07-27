a4417d423dde5a83a8ce42fde1434416171537fc
Falansh Siddh <jasonshaw546@gmail.com>
2020-07-27 15:53:00 +0530

    Updates

---
+++ Section 19 - Django Level Four/learning_templates/basic_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from basic_app import views
+
+app_name = 'basic_app'
+
+urlpatterns = [
+    path('relative/', views.relative, name='relative'),
+    path('other/', views.other, name='other'),
+    path('index/', views.index, name='index'),
+]
