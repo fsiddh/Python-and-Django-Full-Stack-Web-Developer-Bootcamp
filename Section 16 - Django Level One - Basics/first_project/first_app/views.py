7428d5d61c1d30ab1a014ffa61a4064ee774e689
Falansh Siddh <jasonshaw546@gmail.com>
2020-07-27 15:37:24 +0530

    Updates

--- Section 16 - Django Level One - Basics/first_project/first_app/views.py
+++ Section 16 - Django Level One - Basics/first_project/first_app/views.py
@@ -8,3 +8,4 @@ from django.http import HttpResponse
 def index(request):
     my_dict = {'insert_me':"Hello I am from views.py!"}
     return render(request,'first_app/index.html',context=my_dict)
+    
