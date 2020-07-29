fe0dc82cf1083d4a33b59809f8672e6ea4c1081d
Falansh Siddh <jasonshaw546@gmail.com>
2020-07-29 12:49:59 +0530

    Updates

--- Section 1 - Course Introduction/DJANGO_COURSE_2.xx/15-Django_Level_Two/first_project/first_app/views.py
+++ Section 1 - Course Introduction/DJANGO_COURSE_2.xx/15-Django_Level_Two/first_project/first_app/views.py
@@ -18,3 +18,6 @@ def index(request):
     webpages_list = AccessRecord.objects.order_by('date')
     date_dict = {"access_records":webpages_list}
     return render(request,'first_app/index.html',date_dict)
+
+def test(request):
+    return render(request, 'first_app/test.html')
