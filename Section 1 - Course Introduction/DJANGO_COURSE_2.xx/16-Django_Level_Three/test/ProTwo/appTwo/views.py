f35d611de770e9a8514a68e7d63022ca9a3ec2cf
Falansh Siddh <jasonshaw546@gmail.com>
2020-07-30 09:09:58 +0530

    Updates

---
+++ Section 1 - Course Introduction/DJANGO_COURSE_2.xx/16-Django_Level_Three/test/ProTwo/appTwo/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+# from django.http import HttpResponse
+from appTwo.forms import NewUserForm
+# from .models import User
+
+# Create your views here.
+# def index(request):
+#     return HttpResponse("<em>My Second Project</em>")
+
+def help(request):
+    helpdict = {'help_insert':'HELP PAGE'}
+    return render(request,'appTwo/help.html',context=helpdict)
+
+def index(request):
+    return render(request,'appTwo/index.html')
+
+def users(request):
+    form = NewUserForm()
+
+    if request.method == 'POST':
+        form = NewUserForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print(' Invalid entry! ')
+    return render(request, 'appTwo/users.html', {'form':form})        
