e6c25f0c54426f20cad7c8fa7ea8ccd20c6b13fa
fsiddh <jasonshaw546@gmail.com>
2020-07-31 14:42:51 +0530

    update

--- Section 20 - Django Level Five/learning_users/learning_users/settings.py
+++ Section 20 - Django Level Five/learning_users/learning_users/settings.py
@@ -84,6 +84,15 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+]
+
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
