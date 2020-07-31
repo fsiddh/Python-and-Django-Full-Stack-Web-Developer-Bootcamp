72be3fb5c2c69dc100ce124ee833a7a8d373ecba
fsiddh <jasonshaw546@gmail.com>
2020-07-31 16:16:42 +0530

    updates

--- Section 20 - Django Level Five/learning_users/basic_app/models.py
+++ Section 20 - Django Level Five/learning_users/basic_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UserProfileInfo(models.Model):
+
+    user = models.OneToOneField(User)
+
+    portfolio_site = models.URLField(blank=True)
+    profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
+
+    def __str__(self):
+        return self.user.username
