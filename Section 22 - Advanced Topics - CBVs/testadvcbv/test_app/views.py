f3dd0a79408f81fa4eedd0bcd11854c1b4e64384
fsiddh <jasonshaw546@gmail.com>
2020-08-05 14:44:39 +0530

    updates

--- Section 22 - Advanced Topics - CBVs/testadvcbv/test_app/views.py
+++ Section 22 - Advanced Topics - CBVs/testadvcbv/test_app/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import View
+from django.views.generic import View, TemplateView
 from django.http import HttpResponse
 # Create your views here.
 
-class CBView(View):
-    def get(self, request):
-        return HttpResponse('XYZ')
+class IndexView(TemplateView):
+    template_name = 'index.html'
--- Section 22 - Advanced Topics - CBVs/testadvcbv/testadvcbv/urls.py
+++ Section 22 - Advanced Topics - CBVs/testadvcbv/testadvcbv/urls.py
@@ -19,5 +19,5 @@ from test_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.CBView.as_view())
+    path('', views.IndexView.as_view())
 ]
